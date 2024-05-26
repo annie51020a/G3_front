5ee96b6b6c474700447b98a9d86be7a268756249
annie <annie51020a@gmail.com>
2024-05-26 16:46:20 +0800

    section

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,9 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/product">Product</RouterLink>
       </nav>
+      <sectionA />
+      <sectionB />
+      <sectionC />
     </div>
   </header>
 
@@ -23,79 +26,34 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 
 <script>
+// import { defineComponent } from 'vue';
 import { RouterLink, RouterView } from 'vue-router';
 import HelloWorld from './components/HelloWorld.vue';
+import SectionA from './components/home/SectionA.vue';
+import SectionB from './components/home/SectionB.vue';
+import SectionC from './components/home/SectionC.vue';
 
 export default {
   components: {
     RouterLink,
     RouterView,
-    HelloWorld
+    HelloWorld,
+    SectionA,
+    SectionB,
+    SectionC
   }
 }
 </script>
 
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+section {
+  background-color: red;
+  height: 90vh;
+  margin: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 10rem;
 }
 </style>
