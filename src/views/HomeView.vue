664459c3faf00650b5fa67e191d8490679f49b80
christ1113 <class30831@gmail.com>
2024-05-28 13:44:40 +0800

    IndexBanner

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import IndexBanner from '../components/IndexBanner.vue'
 </script>
 
 <template>
   <main>
+    <IndexBanner />
     <TheWelcome />
   </main>
 </template>
