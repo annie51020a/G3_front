b2f2621f7498939f11821dbe5db49c005fa692a8
annie <annie51020a@gmail.com>
2024-05-29 00:13:18 +0800

    footer

--- src/components/home/MainFooter.vue
+++ src/components/home/MainFooter.vue
@@ -1,5 +1,41 @@
 <template>
-    <div>
-        <!-- <p>凱閔大帥哥</p> -->
-    </div>
+    <footer>
+        <div class="container">
+            <div class="brand-info">
+                <div class="footer-logo">
+                    <RouterLink to="/"><img src="../../../pic/footer-logo.png" alt="logo"></RouterLink>
+                </div>
+                <p>傘韻 SanYun</p>
+                <p>地址  桃園市中壢區復興路46號9樓</p>
+                <p>電話  (03) 4251108</p>
+                <p>信箱  Umbrella@gmail.com</p>
+            </div>
+            <nav>
+                <RouterLink to="/about">關於我們</RouterLink>
+                <RouterLink to="/news">最新消息</RouterLink>
+                <RouterLink to="/activity">活動資訊</RouterLink>
+                <RouterLink to="/product">周邊商品</RouterLink>
+                <RouterLink to="/quiz">知識小學堂</RouterLink>
+            </nav>
+            <div class="social-media">
+                <p>社群媒體</p>
+                <div class="social-picture"><RouterLink to="/"><img src="../../../pic/ant-design_facebook-filled.svg" alt=""></RouterLink><RouterLink to="/"><img src="../../../pic/youtube.svg" alt=""></RouterLink></div>
+                <div class="social-picture"><RouterLink to="/"><img src="../../../pic/flowbite_x-solid.svg" alt=""></RouterLink><RouterLink to="/"><img src="../../../pic/ri_instagram-line.svg" alt=""></RouterLink></div>
+            </div>
+        </div>
+        <div class="disclaimer-txt"><p>本網站為緯育TibaMe_前端設計工程師班第89期學員專題成果作品,本平台僅供學習、展示之用。若有抵觸有關著作權,或有第三人主張侵害智慧財產權等情事,均由學員負法律上責任,緯育公司概不負責。若有侵權疑慮,您可以私訊[ 緯育TibaMe ],後續會由專人協助處理。</p></div>
+        <div class="oil-paper-umbrella-picture"><RouterLink to="/"><img src="../../../pic/Ellipse 3.png" alt=""></RouterLink></div>
+        </footer>
 </template>
+
+
+
+<script>
+import { RouterLink } from 'vue-router';
+
+export default {
+    components: {
+        RouterLink,
+    }
+}
+</script>
