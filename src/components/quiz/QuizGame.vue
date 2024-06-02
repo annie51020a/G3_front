b61cd7269ba06693738946699f4153c4e2ede2b0
christ1113 <class30831@gmail.com>
2024-06-02 18:48:36 +0800

    add_quiz_animation

---
+++ src/components/quiz/QuizGame.vue
@@ -0,0 +1,13 @@
+<template>
+    <section class="quiz-game">
+        <div></div>
+    </section>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+
+</style>
--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,3 +1,195 @@
 <template>
-    <main>知識小學堂</main>
+    <section class="quiz">
+
+
+        <!-- 起始動畫 -->
+
+        <div class="quiz-ani-start">
+            <div class="umbrella-ani">
+                <img class="umbrella-ani-img" src="@/assets/pic/quiz/umbrella-ani.png" alt="" />
+
+                <!-- 遊戲規則 -->
+                <div class="quiz-rule">
+                    <div class="frame-start">
+                        <div class="rule-text">
+                            <h3>油紙傘知識小學堂</h3>
+                            <p>遊戲規則<br />題目一共五題，考驗油紙傘小知識<br />答對三題以上即可獲得優惠券</p>
+                            <button @click="startQuiz()">開始遊戲</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- 背景 -->
+        <!-- <div class="quiz-bac">
+            <div class="cloud1">
+                <img src="@/assets/pic/quiz/cloud1.png" alt="cloud" />
+            </div>
+            <div class="cloud2">
+                <img src="@/assets/pic/quiz/cloud2.png" alt="cloud" />
+            </div>
+            <div class="cloud3">
+                <img src="@/assets/pic/quiz/cloud3.png" alt="cloud" />
+            </div>
+            <div class="cloud4">
+                <img src="@/assets/pic/quiz/cloud4.png" alt="cloud" />
+            </div>
+            <div class="cloud5">
+                <img src="@/assets/pic/quiz/cloud5.png" alt="cloud" />
+            </div>
+            <div class="flower1">
+                <img src="@/assets/pic/quiz/flower1.png" alt="flower" />
+            </div>
+            <div class="flower2">
+                <img src="@/assets/pic/quiz/flower2.png" alt="flower" />
+            </div>
+            <div class="kid">
+                <img src="@/assets/pic/quiz/kid.png" alt="kid" />
+            </div>
+        </div> -->
+
+        <!-- 題目內容 -->
+    </section>
 </template>
+
+<script>
+export default {
+    mounted() {
+        const umbrellaAni = document.querySelector('.umbrella-ani')
+        const quizRule = document.querySelector('.quiz-rule')
+        const umbrellaAniImg = document.querySelector('.umbrella-ani-img')
+        const ruleText = document.querySelector('.rule-text')
+
+        setTimeout(() => {
+            umbrellaAni.style.animation = 'none'
+            umbrellaAniImg.style.display = 'none'
+            quizRule.style.width = '80vw'
+            ruleText.style.width = '90%'
+        }, 2000)
+        setTimeout(() => {
+            ruleText.style.display = 'flex'
+            ruleText.style.flexDirection = 'column'
+            ruleText.style.justifyContent = 'center'
+        }, 3600)
+
+    },
+    methods: {
+
+    },
+}
+</script>
+
+<style lang="scss">
+.quiz-ani-start {
+    width: 100%;
+    height: 40vw;
+    overflow: hidden;
+
+    .umbrella-ani img {
+        width: 100%;
+        height: 100%;
+    }
+
+    .umbrella-ani {
+        //雨傘旋轉
+        position: relative;
+        width: 40vw;
+        height: 40vw;
+        border-radius: 50%;
+        background-size: cover;
+        background-repeat: no-repeat;
+        margin: auto;
+        animation: rotate 3s linear infinite;
+    }
+
+    @keyframes rotate {
+        0% {
+            transform: rotate(0deg);
+        }
+
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+
+    .quiz-rule {
+        //遊戲規則
+        position: relative;
+        width: 0;
+        height: 100%;
+        background-color: #e4c7a0;
+        transition: width 2s;
+        z-index: 1;
+        left: 50%;
+        transform: translateX(-50%);
+        border-radius: 20px;
+        display: flex;
+        align-items: center;
+
+        .frame-start {
+            width: 0px;
+            background-color: white;
+            border-radius: 20px;
+            width: 90%;
+            height: 90%;
+            margin: auto;
+            text-align: center;
+            align-items: space-between;
+
+            .rule-text {
+                display: none;
+                border-radius: 20px;
+                width: 90%;
+                height: 90%;
+                margin: auto;
+                text-align: center;
+                align-items: space-between;
+                animation: fadeInFromTop 1s ease forwards;
+
+                >h3 {
+                    color: #515a6e;
+                    margin: 5%;
+                }
+
+                >p {
+                    color: #ff4d00;
+                    line-height: 200%;
+                }
+
+                >button {
+                    background-color: #be1a0e;
+                    border-radius: 20px;
+                    color: white;
+                    font-size: 20px;
+                    width: 140px;
+                    height: 50px;
+                    margin: 5% auto;
+                    border: none;
+
+                    &:hover {
+                        background-color: white;
+                        color: #be1a0e;
+                        border: 1px solid #be1a0e;
+                        transition: 0.3s;
+                    }
+                }
+
+                @keyframes fadeInFromTop {//往下淡入動畫
+                    from {
+                        opacity: 0;
+                        transform: translateY(50px);
+                        /* 向上移動 50px */
+                    }
+
+                    to {
+                        opacity: 1;
+                        transform: translateY(0);
+                        /* 恢復原位置 */
+                    }
+                }
+            }
+        }
+    }
+}
+</style>
