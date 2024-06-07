779638284e7284a45ead429170d902723246c947
hanxinn <elfzhan@gmail.com>
2024-06-07 13:54:10 +0800

    新增商品列表

---
+++ src/components/layout/ProductCard.vue
@@ -0,0 +1,101 @@
+<template>
+    <div v-if="item" class="card">
+        <div class="card-pic">
+            <img v-if="item.image" :src="parseIcon(item.image)" :alt="item.name">
+            <img v-else src="" alt="">
+            <!-- <div class="card-fav">
+                <img class="card-fav-icon" alt="heart">
+            </div> -->
+        </div>
+        <div class="card-txt">
+            <div class="card-tag">
+                <span>{{ item.tag || '' }}</span>
+            </div>
+            <div class="card-title">
+                {{ item.name || '' }}
+            </div>
+            <div class="card-rating">
+                <span v-for="star in Math.floor(item.rating)" :key="star">
+                    🌟
+                </span>
+            </div>
+            <div class="card-price">
+                NT${{ item.price || '' }}
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['item'],
+    methods: {
+        parseIcon(file) {
+            // 指到src || ..的意思是“回到上一層”
+            return new URL(`../../assets/pic/${file}`, import.meta.url).href
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.card {
+    .card-pic {
+        position: relative;
+        margin-bottom: 15px;
+
+        > img {
+            width: 280px;
+            border-radius: 20px;
+            object-fit: cover;
+        }
+
+        .card-fav {
+            position: absolute;
+            right: 10px;
+            bottom: 10px;
+
+            .card-fav-icon {
+                width: 30px;
+            }
+
+        }
+    }
+
+    .card-txt {
+        padding: 5px;
+
+        .card-tag {
+            margin-bottom: 15px;
+
+            > span {
+                background-color: #fff;
+                border-radius: 15px;
+                padding: 2px 10px;
+                font-size: 16px;
+                color: #CB2E27;
+                border: #CB2E27 1px solid;
+            }
+        }
+
+        .card-title {
+            font-size: 20px;
+            font-weight: bold;
+            color: #564A41;
+            margin-bottom: 15px;
+        }
+
+        .card-rating {
+            margin-bottom: 15px;
+        }
+
+        .card-price {
+            font-family: noto sans tc;
+            font-size: 20px;
+            color: #564A41;
+        }
+    }
+
+}
+</style>
--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,3 +1,97 @@
 <template>
-    <main>商品</main>
+    <h1>周邊商品</h1>
+    <div class="container">
+        <div class="row">
+            <div class="product-menu">
+                <div class="product-tag" @click="clear">全部</div>
+                <div class="product-tag" @click="filterTag('圖案')">
+                    圖案
+                </div>
+                <div class="product-tag" @click="filterTag('素色')">
+                    素色
+                </div>
+            </div>
+            <div class="product-window">
+                <div v-if="responseData.length === 0">loading...</div>
+                <div v-else-if="displayData.length === 0">nodata...</div>
+                <div v-else class="display-window">
+                    <ProductCard v-for="item in displayData" :key="item.id" :item="item" />
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+
+<script>
+import ProductCard from '@/components/layout/ProductCard.vue'
+export default {
+    components: {
+        ProductCard
+    },
+    data() {
+        return {
+            responseData: [],
+            displayData: []
+        }
+    },
+    //可以用create也可以用mounted
+    // created() {
+    mounted() {
+        fetch("/products.json")
+            .then(res => res.json())
+            .then(json => {
+                // 確認有沒有response
+                console.log(json);
+                // 備份還原用
+                this.responseData = json
+                // 顯示用
+                this.displayData = json
+            })
+    },
+    methods: {
+        clear() {
+            this.displayData = this.responseData
+        },
+        filterTag(tagName) {
+            this.displayData = this.responseData.filter((item) => {
+                return item.tag === tagName
+            })
+        }
+    }
+}
+
+</script>
+
+<style lang="scss">
+h1 {
+    text-align: center;
+    color: #564A41;
+    margin-bottom: 50px;
+}
+.container {
+    max-width: 1200px;
+    width: 100%;
+    margin: 0 auto;
+    .row {
+        display: flex;
+        .product-menu {
+            width: 25%;
+            .product-tag {
+                margin-bottom: 10px;
+                cursor: pointer;
+                color: #564A41;
+            }
+
+    }
+    .product-window {
+        width: 75%;
+        .display-window {
+            display: flex;
+            gap: 25px;
+            flex-wrap: wrap;
+        }
+    }
+    }
+ 
+}
+</style>
