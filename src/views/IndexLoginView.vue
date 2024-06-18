c836fcf18ceb801e3a9e015ca6ec0d492291e294
christ1113 <class30831@gmail.com>
2024-06-18 19:31:34 +0800

    merge

--- src/views/IndexLoginView.vue
+++ src/views/IndexLoginView.vue
@@ -123,7 +123,7 @@ export default {
         }
     },
     mounted() {
-        fetch("/public/member.json")
+        fetch(`${import.meta.env.BASE_URL}/public/member.json`)
             .then(res => res.json())
             .then(json => {
                 console.log(json);
